c865a031677bed3d63da3f1d47b6da4ada152dff
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-12-01 13:45:55 +0530

    Add files via upload

---
+++ JDBC_demo/src/com/Student/Helper.java
@@ -0,0 +1,32 @@
+package com.Student;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Helper {
+
+		static {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+			}
+			catch(Exception e){
+				System.out.println(e);
+				e.printStackTrace();
+			}
+		}
+		
+		public static Connection con() throws SQLException{
+			return DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb","root","567890");
+		}
+		public static void main(String args[]) throws SQLException{
+			Menudriven md= new Menudriven();
+			md.saveMenudriven();
+			md.fetchMenudriven();
+			md.updateMenudriven();
+			md.deleteMenudriven();
+		
+		}
+	}
+
+

294153eb0a47cf4d6d48f9e183cd88ceafd93e00
Sarbojit199 <108295218+Sarbojit199@users.noreply.github.com>
2022-12-04 14:11:33 +0530

    Add files via upload

---
+++ Banking_Management_System/MenuDriven.java
@@ -0,0 +1,62 @@
+package com.bank;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuDriven {
+	
+	public static void main(String[] args) {
+		BMS_Loader bms=new BMS_Loader();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("******Welcome to Anonymous Bank******");
+		while(true) {
+			System.out.println("[1.Create Account],"
+					+ "[2.Display Details],"
+					+ "[3.Deposit],"
+					+ "[4.Withdraw],"
+					+"[5.Change Pin],"
+					+ "[0.Exit]");
+			int choice=sc.nextInt();
+			while(!(choice>=0 && choice<6)) {
+				System.out.println("!!Invalid choice!!\nTry Again : ");
+				choice=sc.nextInt();
+			}
+			if(choice==0) {
+				System.out.println("******!!Thank you for choosing our bank!!******");
+				break;
+			}else {
+				try {
+					if(choice==1) {
+						bms.storeData();
+					}else if(choice==2) {
+						bms.displayData();
+					}else if(choice==3 || choice==4 || choice==5) {
+						bms.updateData(choice);
+					}
+				}catch(InputMismatchException e) {
+					System.out.println("!!Invalid entry!!");
+				}catch(Exception e) {
+					System.out.println(e.getMessage());
+				}finally {
+					System.out.println("[1-->Main Menu],[0-->Exit]");
+					int choice1=sc.nextInt();
+					while(!(choice1==0||choice1==1)) {
+						System.out.println("!!Invalid choice!!\nTry Again : ");
+						choice1=sc.nextInt();
+					}
+					if(choice1==1) {
+						continue;
+					}else if(choice1==0) {
+						System.out.println("******!!Thank you for choosing our bank!!******");
+						break;
+					}
+				}
+			}
+		}
+	}
+
+}
+
+
+
+
